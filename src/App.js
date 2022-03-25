48c433e4b9cb82485f2e0377090817aa5de31db3
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-25 15:58:30 +0500

    Get redux state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Greeting from './components/Greeting';
+import Greetings from './components/Greetings';
+import { fetchGreetings } from './redux/actions';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, []);
+  const stateData = useSelector((state) => state.Reducer.greetings);
+  console.log('Store Data: ', stateData);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <h1>
+                  <Link to="greetings">Greetings</Link>
+                </h1>
+              }
+            />
+            <Route path="/greetings" element={<Greetings />} />
+            <Route path="/greetings/:greetingId" element={<Greeting />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
-}
+};
 
 export default App;
